7bf5e373de316672463305f51a82fd4bf45414cd
Vinod Kumar <vinodkumar@Vinods-MacBook-Air.local>
2022-08-09 12:38:44 +0530

    pending

--- src/main/java/com/spring/quickstarts/config/SpringWebAppInitializer.java
+++ src/main/java/com/spring/quickstarts/config/SpringWebAppInitializer.java
@@ -1,4 +1,4 @@
-package com.spring.quickstarts.controllers;
+package com.spring.quickstarts.config;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -7,8 +7,6 @@ import javax.servlet.ServletRegistration;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-
-import com.spring.quickstarts.config.MvcConfig;
 
 public class SpringWebAppInitializer implements WebApplicationInitializer {
 
