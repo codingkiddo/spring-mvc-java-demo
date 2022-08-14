5cc16a07dcfeeb455c2728c7f85d4d378094878a
Vinod Kumar <vinodkumar@Vinods-MacBook-Air.local>
2022-08-14 16:30:26 +0530

    pending

---
+++ src/main/java/com/spring/quickstarts/config/RootConfig.java
@@ -0,0 +1,13 @@
+package com.spring.quickstarts.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan(basePackages = { "com.example" }, excludeFilters = {
+		@Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class) })
+public class RootConfig {
+}
--- src/main/java/com/spring/quickstarts/controllers/GreetController.java
+++ src/main/java/com/spring/quickstarts/controllers/GreetController.java
@@ -1,38 +1,23 @@
+
 package com.spring.quickstarts.controllers;
 
+import javax.naming.NamingException;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
 @RequestMapping("greet")
 public class GreetController {
 
-//    @RequestMapping(method = RequestMethod.GET)
-//	Annotation for mapping HTTP GET requests onto specific handler methods.
-//	Specifically, @GetMapping is a composed annotation that acts as a shortcut 
-//	for @RequestMapping(method = RequestMethod.GET).
 	
-	@GetMapping("/")
-    public String getInitialMessage() {
-        return "greet";
+	@RequestMapping(method = RequestMethod.GET)
+    public String getInitialMessage() throws NamingException {
+		
+        return "home";
     }
 
-//    @RequestMapping(method = RequestMethod.POST)
-	@PostMapping
-    public String getGreeting(@RequestParam("username") String username) {    
-        if (username != null) {
-            return "hello";
-        } else {
-            return "No such user exists! Use 'emuster' or 'jdoe'";
-        }
-    }
-	
-	@GetMapping("/admin") 
-	public String admin() { 
-		return "admin"; 
-	} 
+   
 }
