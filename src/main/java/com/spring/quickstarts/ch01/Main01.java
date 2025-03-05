39bc672f2cee6083c61a8248e21ee8f54e50a03c
codingkiddo <codingkiddo@gmail.com>
2025-03-05 09:10:36 +0530

    ch01
    Change-Id: I536f0dcd2defe9fb25b0b62cced9d6cb2606afa8

---
+++ src/main/java/com/spring/quickstarts/ch01/Main01.java
@@ -0,0 +1,28 @@
+package com.spring.quickstarts.ch01;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.spring.quickstarts.ch01.beans.Parrot;
+import com.spring.quickstarts.ch01.config.ProjectConfig;
+
+public class Main01 {
+
+	public static void main(String[] args) {
+
+		BeanFactory context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+		
+		Parrot parrot = context.getBean(Parrot.class);
+		System.out.println(parrot.getName());
+		
+		String hello = context.getBean(String.class);
+		System.out.println(hello);
+		
+		Integer ten = context.getBean(Integer.class);
+		System.out.println(ten);		
+		
+		( (AnnotationConfigApplicationContext) context ).close();
+		
+	}
+
+}
